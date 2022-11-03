9094984806842a000af4db51c9133423e015c786
NicolasPotelJunot <nicolaspoteljunot@gmail.com>
2022-11-03 00:29:52 -0300

    finish logic code calendar

--- src/components/layout/tools/calendar/CalendarView.jsx
+++ src/components/layout/tools/calendar/CalendarView.jsx
@@ -1,11 +1,34 @@
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import { Dayjs } from "dayjs";
+import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+import { useState } from "react";
 
+const isWeekend = (date) => {
+  const day = date.day();
+
+  return day === 0 || day === 6;
+};
 
 export const CalendarView = () => {
+  const [value, setValue] = useState(Dayjs);
+
   return (
-    <div className="bg-white my-10 w-96">
-       <DatePicker />
+    <div className="w-1/3 h-1/2">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <StaticDatePicker
+        displayStaticWrapperAs="desktop"
+          orientation="landscape"
+          openTo="day"
+          value={value}
+          shouldDisableDate={isWeekend}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
     </div>
-  )
-}
+  );
+};
