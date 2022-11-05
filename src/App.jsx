509108bb45d26096ce48df13edd8034e9dce7143
NicolasPotelJunot <nicolaspoteljunot@gmail.com>
2022-11-05 01:51:48 -0300

    deploy 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './styles/index.css'
-import 'animate.css';
-import AppRoutes from './routes/appRoutes.jsx'
 // redux
 import { store } from './stateManagement/store/store'
 import { Provider } from 'react-redux'
 // persist
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
+
+import AppRoutes from './routes/appRoutes.jsx'
+import './styles/index.css'
+import 'animate.css';
 
 let persistor = persistStore(store)
 
