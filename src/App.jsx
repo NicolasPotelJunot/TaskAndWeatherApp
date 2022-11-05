79d77dfdf345328bde66a15fd7fe90cfe6668cca
NicolasPotelJunot <nicolaspoteljunot@gmail.com>
2022-11-05 02:01:43 -0300

    deploy 6

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,13 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
 
-import AppRoutes from './routes/appRoutes.jsx'
+import AppRoutes from './routes/AppRoutes.jsx'
 import './styles/index.css'
 import 'animate.css';
 
 let persistor = persistStore(store)
 
 export const App = () =>{
-
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
