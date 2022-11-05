a2683c7887f58e5ed076d26d17476befac9e4558
NicolasPotelJunot <nicolaspoteljunot@gmail.com>
2022-11-05 01:39:39 -0300

    deploy 3

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { DashRoutes } from "./DashRoutes";
 
 function AppRoutes() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
         <DashRoutes />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
